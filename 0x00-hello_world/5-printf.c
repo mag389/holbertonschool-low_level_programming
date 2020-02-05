8a8001e46d014a398190f954865afcbf552c8b35
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-05 12:42:59 -0500

    changed comments betty compliant

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+/* first use of printf file */
 
+/**
+*main - prints phrase then returns 0
+*
+*description:returns int
+*
+*main: prints phrase and returns 0
+*
+*Return: reutns int 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
-	return(0);
+
+	return (0);
 }
