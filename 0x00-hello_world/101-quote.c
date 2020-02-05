81f2db6d8e1cc794f5f06ecc7e193e77be68d016
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-05 14:48:40 -0500

    trying it with a loop

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	while (*s) putchar(*s++);
 	return (1);
 
 }
