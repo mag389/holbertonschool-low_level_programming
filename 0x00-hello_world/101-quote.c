739e9bfdf7db7e25cfa657ca69ad0bc041c74e00
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-05 14:57:18 -0500

    fixed betty format

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-int main()
+/**
+*main- prints the quote returns zero
+*
+*Main: returns type int
+*
+*Description: prtins a quote and returns 0
+*Return: type int
+*/
+int main(void)
 {
-	char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	while (*s) putchar(*s++);
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	while (*s)
+		putchar(*s++);
 	return (1);
 
 }
