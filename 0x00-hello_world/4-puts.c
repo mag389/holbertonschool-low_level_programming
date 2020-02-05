420c6c4a30b5cfd0651825b58ebc9f19b73e95cb
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-05 12:22:38 -0500

    only changed commetns for Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+/* 4-puts.c printing with puts */
 
+/**
+*prints the phrase below using the puts method
+*then prints 0
+*
+*main: the main function of this file
+*return: just returns integer zero
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
