228b914ea366bf31dbd57051369277ba6decb4cd
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-06 15:50:20 -0500

    prints numbers with commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main-prints all 1 digit nums
+*
+*description:prints single digit nums
+*Return:0
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
