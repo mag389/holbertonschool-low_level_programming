8363241c152a4c256dda32108ebd9f22c9e54a8d
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-06 15:41:31 -0500

    prints 0-15 in base hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main-prints all 1 digit nums
+*
+*description:prints single digit nums
+*Return:0
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar('0' + i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
