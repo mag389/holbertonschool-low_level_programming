c979c053413705997f5bd962918b4e8d92681abf
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-06 15:04:07 -0500

    prints both cases of alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints whole alphabet in noth cases
+*
+*Description:returns entire alphabet
+*Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
