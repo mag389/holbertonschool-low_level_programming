704db2a0280dfc7ccb57ad9d2c60b4bd8932ad36
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-06 14:28:27 -0500

    capitalize part of string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("last digit of %d is ", n);
+	printf("Last digit of %d is ", n);
 	if (n % 10 > 55)
 	{
 		printf("%d and is greater than 5\n", n % 10);
