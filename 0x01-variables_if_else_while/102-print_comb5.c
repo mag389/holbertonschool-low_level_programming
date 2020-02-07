48392342b94b1c37baf5386922082b89e306f55c
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-06 19:31:01 -0500

    douvle digit combos of 2

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main-prints all 2 digit nums
+*
+*description:prints double digit nums
+*Return:0
+*
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+		putchar('0' + i / 10 );
+		putchar('0' + i % 10);
+		putchar(' ');
+		putchar('0' + j / 10);
+		putchar('0' + j % 10);
+		if (i < 98 || j < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
