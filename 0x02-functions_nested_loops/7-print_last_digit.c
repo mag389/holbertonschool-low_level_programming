881462ae294192e1535c1fe1dfaf0d41a70c5c78
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-10 15:28:53 -0500

    print last digit of number even if negative

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+*print_last_digit- tells absolute val of num
+*
+*Description:returns the last digit of n
+*Return:last digit of n
+*
+*@n: a number
+*/
+int print_last_digit(int n)
+{
+	if (n > 0)
+	{
+		_putchar('0' + (n % 10));
+		return (n % 10);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('0' + (-1 * n % 10));
+		return (-1 * n % 10);
+	}
+}
