fffe4401a2d303c7b50c7ca653f9497b737d450c
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-10 16:19:25 -0500

    adds tgether two ints from argument

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+*add- add the two numbers return their sum
+*
+*Description:add the numbers
+*Return:sum
+*@i: a number
+*@j:another num
+*
+*/
+int add(int i,int j)
+{
+	return (i + j);
+}
