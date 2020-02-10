1a1bc7e4c5638c041f4f3ec1307a6ef0837d8120
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-10 16:08:46 -0500

    prints out the 9's table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+*times_table- prints the times table of 9
+*
+*Description:print the table
+*Return:nothing
+*
+*
+*/
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+		{
+		for (j = 0; j < 10; j++)
+			{
+			if (i * j > 10)
+			{
+				_putchar('0' + (j * i) / 10);
+				_putchar('0' + (j * i) % 10);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + j * i);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			}
+		_putchar('\n');
+		}
+}
