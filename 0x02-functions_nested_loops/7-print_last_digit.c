8246a58079daee6808015afa0d20898bc24cec7e
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-11 09:50:54 -0500

    using one conditional

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,14 +16,15 @@ int print_last_digit(int n)
 		_putchar('0' + ((-1 * n) % 10));
 		return ((-1 * n) % 10);
 	}
-	else if (n > 0)
+	else
 	{
 		_putchar('0' + (n % 10));
 		return (n % 10);
 	}
-	else
+/*	else
 	{
 		_putchar('0');
 		return (0);
 	}
+*/
 }
