b44cc3daaba2055fa8e51f58bd6c4b72b9fffd46
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-11 09:49:14 -0500

    rearranged conditional statements

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,15 +11,15 @@
 */
 int print_last_digit(int n)
 {
-	if (n > 0)
-	{
-		_putchar('0' + (n % 10));
-		return (n % 10);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('0' + ((-1 * n) % 10));
 		return ((-1 * n) % 10);
+	}
+	else if (n > 0)
+	{
+		_putchar('0' + (n % 10));
+		return (n % 10);
 	}
 	else
 	{
