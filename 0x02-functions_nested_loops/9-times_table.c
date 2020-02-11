49f16206835bd1a63506d679b9e0cf381dbcbbfe
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-11 09:45:54 -0500

    changed conditional to <= and removed space before first num

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,11 @@ void times_table(void)
 		{
 		for (j = 0; j < 10; j++)
 			{
-			if (i * j > 10)
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (i * j >= 10)
 			{
 				_putchar('0' + (j * i) / 10);
 				_putchar('0' + (j * i) % 10);
