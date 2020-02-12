e8e724f8aaf8800e48eab612aa25615efb59488c
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-11 19:23:29 -0500

    fixed telling if it's a leap year,not ege case

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,5 +5,9 @@
 
 void positive_or_negative(int i);
 
+void print_remaining_days(int a, int day, int year);
+
+int convert_day(int a, int b);
+
 #endif /* HOLBERTON_H */
 int largest_number(int a, int b, int c);
