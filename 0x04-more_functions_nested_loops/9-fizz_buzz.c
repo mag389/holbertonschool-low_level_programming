76024773617edfbdb45eb26a86e8000f76e4c297
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-13 10:31:49 -0500

    famous fizzbuzz problem. tested local

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+*main- prints nums
+*
+*Description:prints nums 0-100 multiples of 3 get fizz
+*	multiples of 5 get buzz, both get fizzbuzz
+*Return:0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
