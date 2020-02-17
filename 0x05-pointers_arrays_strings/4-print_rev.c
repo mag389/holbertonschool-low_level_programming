aafa343d9047f55b0ed589aeadccb06fe2ac7711
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-17 13:57:52 -0500

    reverses a string and prints

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*print_rev - prints a reversed string
+*
+*Description:takes a string. reverses. prints
+*Return:void
+*@s:given string
+*/
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = _strlen(s);
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+/*
+*	while (*(str + len) != '\0')
+*		{
+*		_putchar(*(str + len));
+*		len++;
+*		}
+*/
+	_putchar('\n');
+}
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*_strlen - gives length of a given string
+*
+*Description:takes a pointer to a char and returns length
+*Return:the length of a string
+*@s:given integer variable 1
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		{
+		len++;
+		}
+	return (len);
+}
