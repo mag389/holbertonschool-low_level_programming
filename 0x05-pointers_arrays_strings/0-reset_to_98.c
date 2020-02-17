143dc2186b487272e63be35b77f36b8d5b43a4bb
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-17 10:49:00 -0500

    resets value at mem address to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*reset_to_98 - resets pointer to 98
+*
+*Description:changes variable being pointed at to 98
+*Return:void
+*@n:given integer variable
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
