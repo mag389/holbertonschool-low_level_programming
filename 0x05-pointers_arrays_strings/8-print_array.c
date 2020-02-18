588507fe3a033a809dec47347c9aa864d8eeb92f
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-18 10:37:13 -0500

    changing how i deal with last comma

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
-		printf("%i, ", a[i]);
-	printf("%d\n", a[n - 1]);
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%i, ", a[i]);
+		}
+		else
+			printf("%d\n", a[n - 1]);
+	}
 }
