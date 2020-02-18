03656f79a118a0ae68d4c3e5fb9175e35d3852e9
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-17 23:06:16 -0500

    copies string from source to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+*_strcpy - copies string
+*
+*Description: copies string from src to dest
+*Return: the pointer to dest
+*@dest: the destination
+*@src: the source string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
