b2a57506e200693a6679c07e9f9e3a73d2553660
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-18 08:38:28 -0500

    going back to original method

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*rev_string - reverses string
+*
+*Description:takes a string. reverses it
+*Return:void
+*@s:given string
+*/
+void rev_string(char *s)
+{
+	int len = _strlen(s);
+	int i;
+	char temp2[len];
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		temp2[len - i - 1] = s[i];
+	}
+	for (i = 0; i < len; i++)
+		s[i] = temp2[i];
+}
+
+/**
+*_strlen - gives length of a given string
+*
+*Description:takes a pointer to a char and returns length
+*Return:the length of a string
+*@s:given integer variable 1
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		{
+		len++;
+		}
+	return (len);
+}
