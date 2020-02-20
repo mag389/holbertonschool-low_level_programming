61af52a8763faf19177fd8eb9624179c37c9e91d
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-20 16:59:13 -0500

    changes value of a without using a

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -16,9 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  ;
-  printf("p is: %p\n",p);
-  printf("n is:%i\n",n);
+  *(p + 4) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
