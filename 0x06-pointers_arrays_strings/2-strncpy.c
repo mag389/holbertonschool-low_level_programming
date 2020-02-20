b65462971c70323ba8535c19548c00c4a764ab27
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-20 12:52:42 -0500

    trying with removed var

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,23 +4,24 @@
 /**
 * _strncpy - copies first n byts from one string
 *Return: pointer to resulting str dest
-*@src:string to append first n to end of other
+*@src:string to copy first n into other
 *@dest:where to copy to
 *@n: number of elements from source to copy to dest
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int lensrc = 0, lendest = 0, i, newlength;
+	int lensrc = 0, lendest = 0, i;
 
 	while (*(src + lensrc) != 0)
 		lensrc++;
 	while (*(dest + lendest) != 0)
 		lendest++;
-	if (n < lensrc)
-		newlength = lendest + n;
-	else
-		newlength = lendest + lensrc;
-
+/*
+*	if (n < lensrc)
+*		newlength = n;
+*	else
+*		newlength = lensrc;
+*/
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
