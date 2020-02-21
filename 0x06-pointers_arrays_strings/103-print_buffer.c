cddda56278d8b8b93f977459dbf943757d86a397
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-21 00:07:59 -0500

    closer but not there yet

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -14,10 +14,10 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i < size; i += 10)
 	{
-		printf("%00x: ", i);
+		printf("%08x: ", i);
 		for (j = 0; j < 10 && j < size; j += 2)
 		{
-			printf("%x%x ", b[i + j], b[i + j]);
+			printf("%02x%02x ", b[i + j], b[i + j + 1]);
 		}
 		for (j = 0; j < 10 && j < size; j++)
 		{
