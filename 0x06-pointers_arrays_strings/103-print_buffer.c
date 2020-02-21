7fc23f662750ab59d37d859bb32bbaea6ece3b2b
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-21 00:22:17 -0500

    fixing if size <= 0

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -12,6 +12,10 @@ void print_buffer(char *b, int size)
 {
 	int i, j;
 
+	if (size <= 0)
+		printf("\n");
+	else
+	{
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
@@ -30,5 +34,6 @@ void print_buffer(char *b, int size)
 				printf("%c", b[i + j]);
 		}
 		printf("\n");
+	}
 	}
 }
