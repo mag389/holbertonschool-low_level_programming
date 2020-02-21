8881d0bdb548c3da757b095d2b4bf3fdfb436cea
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-20 23:12:02 -0500

    changing return statement and making betty happy

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -12,17 +12,16 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int n1len = 0, n2len = 0, d1, d2, smallsum, remainder = 0, i;
-	int smallerlen = 0, j = 0;
+	int n1len = 0, n2len = 0, d1, d2, smallsum, remder = 0, i, j = 0, minlen = 0;
 
 	while (*(n1 + n1len) != '\0')
 		n1len++;
 	while (*(n2 + n2len) != '\0')
 		n2len++;
 	if (n1len < n2len)
-		smallerlen = n1len;
+		minlen = n1len;
 	else
-		smallerlen = n2len;
+		minlen = n2len;
 	if (n1len >= size_r - 1 || n2len >= size_r - 1)
 		return (0);
 	if (n1len == n2len && n1len >= size_r - 1)
@@ -40,16 +39,17 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			d2 = n2[n2len - i - 1] - 48;
 		else
 			d2 = 0;
-		smallsum = d1 + d2 + remainder;
+		smallsum = d1 + d2 + remder;
 		r[size_r - i] = smallsum % 10 + 48;
 		if (smallsum >=  10)
-			remainder = 1;
+			remder = 1;
 		else
-			remainder = 0;
-		if (i == size_r - 1 && remainder == 1)
+			remder = 0;
+		if (i == size_r - 1 && remder == 1)
 			return (0);
 	}
 	while (!(r[j] >= '1' && r[j] <= '9'))
 		j++;
-	return (&r[j]);
+	r = &r[j];
+	return (r);
 }
