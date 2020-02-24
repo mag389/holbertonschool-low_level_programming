b1d6109569629d3d6d1a2fe68b037187836dcce4
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-24 15:45:31 -0500

    prints chessboard from 2d array

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+*print_chessboard - prints the chessboard
+*Return: void
+*@a:string to look through
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		putchar('\n');
+	}
+}
