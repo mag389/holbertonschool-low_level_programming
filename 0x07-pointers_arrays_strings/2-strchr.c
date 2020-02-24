97c95c4573525b8449877c26a6da8dafb0a0a9de
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-24 13:39:02 -0500

    search for char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+*_strchr - locates character in string
+*Return: pointer to first occurence of c in s or null
+*@s:string to search through
+*@c: char to look for
+*/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+			{
+			return (&s[i]);
+			}
+		i++;
+	}
+	return (0);
+}
