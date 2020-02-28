69c4e6e4771cb699912405e53338a2b5b7fb30b4
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-02-28 14:24:40 -0500

    this file multiples the two arguments recieved, or error if <2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+*main - a program that prints all arguments passed
+*Return: 0
+*@argc:size of argv array
+*@argv:arguments passed to program
+*/
+
+int main(int argc, char *argv[])
+{
+	int int1, int2;
+	
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+	int1 = atoi(argv[1]);
+	int2 = atoi(argv[2]);
+	printf("%i\n", int1 * int2);
+	}
+	return (0);
+}
