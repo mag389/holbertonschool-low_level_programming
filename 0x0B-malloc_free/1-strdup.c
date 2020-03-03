1beedb2e9bf277431eb57c69b0927c457a067353
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-03-03 13:31:55 -0500

    removed check for null str

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,13 +11,8 @@ char *_strdup(char *str)
 	int i, size = 0;
 	char *thearray;
 
-	if (str == NULL)
-		return (NULL);
-
 	while (str[size] != 0)
 		size++;
-	if (size == 0)
-		return (NULL);
 
 	thearray = malloc(size * sizeof(str[0]));
 	if (thearray == NULL)
