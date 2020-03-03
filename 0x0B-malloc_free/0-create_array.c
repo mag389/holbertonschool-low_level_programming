d0587af7f92dbc51fb8749c5bdb31a750c11066a
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-03-03 12:45:31 -0500

    adding null return if malloc fails

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	thearray = malloc(size * 2);
-
+	if (thearray == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		thearray[i] = c;
