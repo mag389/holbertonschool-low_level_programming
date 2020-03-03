14e7d27ab6ee2f8a517c18e80d56b6cf23e1a5ad
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-03-03 13:30:27 -0500

    adding check for empty string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,15 +11,18 @@ char *_strdup(char *str)
 	int i, size = 0;
 	char *thearray;
 
-	if (str == NULL || str[0] == NULL)
+	if (str == NULL)
 		return (NULL);
 
 	while (str[size] != 0)
 		size++;
+	if (size == 0)
+		return (NULL);
 
 	thearray = malloc(size * sizeof(str[0]));
 	if (thearray == NULL)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		thearray[i] = str[i];
