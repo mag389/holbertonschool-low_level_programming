4794fec160f5ace4967b4ee07fc5f3a464aa9b94
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-03-03 14:53:38 -0500

    changing computer to try valgrind

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*free_grid - prints a grid of integers
+*Return:char* or null
+*@grid: the grid
+*@height: height of the grid
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
