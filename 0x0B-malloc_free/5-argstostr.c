9847cba13c03ed9304a612a6366e2e912f43f62a
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-03-03 15:21:29 -0500

    adding file that concatenates all args

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+*argstostr - concatenates all args
+*Return: pointer to new string or null
+*@ac:number of elements
+*@av: 2d pointer to elements
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, sizeofarg = 0, totalarglength = 0;
+	char *newstring;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		while (av[i][sizeofarg] != 0)
+			sizeofarg++;
+		totalarglength += sizeofarg * sizeof(char);
+	}
+	newstring = malloc(totalarglength + ac * 2);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != 0; j++)
+		{
+			newstring[k] = av[i][j];
+			k++;
+		}
+		newstring[k] = '\n';
+		k++;
+	}
+	return (newstring);
+}
