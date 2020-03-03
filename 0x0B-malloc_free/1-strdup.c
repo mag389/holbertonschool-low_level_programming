f9974046e8386d0404b4a7eefe717569fbad51e7
Michael George <funkyg1993@michaels-MacBook-Pro.local>
2020-03-03 12:56:19 -0500

    fixing return on malloc fail

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*_strdup - creates array
+*Return:char or null
+*@str:the string to copy from
+*/
+char *_strdup(char *str)
+{
+	int i, size = 0;
+	char *thearray;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[size] != 0)
+		size++;
+
+	thearray = malloc(size * 2);
+	if (thearray == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		thearray[i] = str[i];
+	}
+	thearray[i] = 0;
+
+	return (thearray);
+}
