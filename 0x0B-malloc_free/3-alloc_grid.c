b9b1e7b100c5a14aa1f4ea1722fa3dc1669ffbd5
Michael George <mag389@cornell.edu>
2020-03-03 22:00:04 +0000

    switching width and hieght vars, without typos

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	thegrid = malloc(sizeof(int *) * widtht + 1);
+	thegrid = malloc(sizeof(int *) * width + 1);
 	if (thegrid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
