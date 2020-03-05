a7acf8f38cae102d36bc948db8b6f9b1fe16f956
Michael George <mag389@cornell.edu>
2020-03-05 18:45:05 +0000

    actually making the memory to zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,15 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *thearray;
+	char *thearray;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	thearray = malloc(nmemb * size);
 	if (thearray == NULL)
 		return (NULL);
-	return (thearray);
+	for (i = 0; i < nmemb; i++)
+		thearray[i] = 0;
+	return ((void *)thearray);
 }
