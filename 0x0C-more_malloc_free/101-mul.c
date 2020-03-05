44f3ffd7acdfee3903c0355221e0491b2e2a3f01
Michael George <mag389@cornell.edu>
2020-03-05 20:47:20 +0000

    just a skeleton file for some testing purposes

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/*
+*main - multiplies two numbers
+*Return: o
+*@argc: the size of argv
+*@argv:numbers to multiply
+*/
+
+int main(int argc, char *argv[])
+{
+	int int1, int2;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		exit(98);
+	}
+	else
+	{
+	int1 = atoi(argv[1]);
+	int2 = atoi(argv[2]);
+	printf("%i\n",int1 * int2);
+	}
+	return (0);
+}
