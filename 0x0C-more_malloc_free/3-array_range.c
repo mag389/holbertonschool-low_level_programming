5e5e997545ff863ffd3f7a169ddec8fbbb14466e
Michael George <mag389@cornell.edu>
2020-03-05 18:00:39 +0000

    first commit of array range, betty'd, tested with given example.

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+*array_range - make array of subsequent numbers
+*Return: the int array
+*@min: the starting number (inclusive)
+*@max: the last number (inclusive)
+*/
+int *array_range(int min, int max)
+{
+	int i;
+	int *thearray;
+
+	if (min > max)
+		return (NULL);
+	thearray = malloc(sizeof(int) * (max - min + 2));
+	if (thearray == NULL)
+		return (NULL);
+
+	for (i = 0; i + min <= max; i++)
+		thearray[i] = min + i;
+	thearray[i] = '\0';
+	return (thearray);
+}
