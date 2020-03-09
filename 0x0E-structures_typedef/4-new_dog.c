fab69d607fc9cc2352b48a14986bb641dbf109e7
Michael George <mag389@cornell.edu>
2020-03-09 17:12:38 +0000

    checks for failure for each variable and uses copy of each for dog's new variables

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
 	char *newname, *newowner;
+	int i;
 
 	newdog = malloc(2 * sizeof(char *) + sizeof(int));
 	if (newdog == NULL)
 		return (NULL);
-	newname = name;
-	newowner = owner;	
+
+	newname = malloc(sizeof(name));
+	if (newname == NULL)
+		{
+		free(newdog);
+		return (NULL);
+		}
+	for (i = 0; name[i] != '\0'; i++)
+		newname[i] = name[i];
+	newname[i] = 0;
+
+	newowner = malloc(sizeof(owner));
+	if (newowner == NULL)
+		{
+		free(newname);
+		free(newdog);
+		return (NULL);
+		}
+	for (i = 0; owner[i] != 0; i++)
+		newowner[i] = name[i];
+	newowner[i] = 0;
+
 	newdog->name = newname;
 	newdog->age = age;
 	newdog->owner = newowner;
