1edc8d702939f43522293fdce3dfe5fc417f1cbc
Michael George <mag389@cornell.edu>
2020-03-09 15:04:25 +0000

    adding header file with structure in palce

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - dog with some info
+*@name: the dog's name
+*@age: the dog's age
+*@owner: the og's owner's name
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+
+#endif
