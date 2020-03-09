06b5e2f1283dabf438d042a77b3d1a83f0fd157b
Michael George <mag389@cornell.edu>
2020-03-09 17:04:08 +0000

    fixing file to use copies of name and owner, instead of directly using args

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,13 +12,16 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	char *newname, *newowner;
 
 	newdog = malloc(2 * sizeof(char *) + sizeof(int));
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name;
+	newname = name;
+	newowner = owner;	
+	newdog->name = newname;
 	newdog->age = age;
-	newdog->owner = owner;
+	newdog->owner = newowner;
 
 	return (newdog);
 }
