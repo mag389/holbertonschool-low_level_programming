5e50c710d4e70d9185b38e6e43cea281f6ae1031
Michael George <mag389@cornell.edu>
2020-03-09 16:17:09 +0000

    adding file 1, initiailzes dog structure with three fields.

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*init_dog - initializes dog structure
+*Return: void
+*@d: the pointer dog object
+*@name: name of the dog
+*@age: how old is the dog
+*@owner: name of the owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
