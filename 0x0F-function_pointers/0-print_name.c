3dcbcffa421d2cb2906856570484dc0af8da0e78
Michael George <mag389@cornell.edu>
2020-03-11 15:46:54 +0000

    adding first file that prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+*print_name - prints a name
+*Return: void
+*@name: the name to be printed
+*@f:the function to do to name
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
