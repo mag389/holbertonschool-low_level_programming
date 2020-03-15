e7c96b97cc2018f3eadd94e7b1b959fa94cd4a09
Michael George <mag389@cornell.edu>
2020-03-15 18:56:43 +0000

    again fixed case where needed to print next line without weird stuff happening

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,10 +20,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	else
 		newsep = separator;
 	va_start(list, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%s%s", va_arg(list, char*), newsep);
+		if (i < n - 1)
+		{
+			printf("%s%s", va_arg(list, char*), newsep);
+		}
+		else
+			printf("%s",va_arg(list, char*));
 	}
-	printf("%s\n", va_arg(list, char*));
+	printf("\n");
 	va_end(list);
 }
