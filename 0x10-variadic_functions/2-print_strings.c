52b2a5ea9162246336eb490aa3f4a5d4b40394a3
Michael George <mag389@cornell.edu>
2020-03-15 18:57:22 +0000

    betty formatting, added a space before a comma

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s%s", va_arg(list, char*), newsep);
 		}
 		else
-			printf("%s",va_arg(list, char*));
+			printf("%s", va_arg(list, char*));
 	}
 	printf("\n");
 	va_end(list);
