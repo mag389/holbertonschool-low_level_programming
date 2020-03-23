57c2ea1ae0d6be8e9035305fc3a782e50ca70b6c
Michael George <mag389@cornell.edu>
2020-03-23 19:28:30 +0000

    deletes a node at an index, tested, betty compliant, but ugly code

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+* delete_nodeint_at_index - deletes a specific node
+* Return: 1 for success, -1 for failure
+* @index: index of the node that shold be deleted
+* @head: pointer to head node of list
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *todel;
+
+	if (index == 0)
+	{
+		if (*head == NULL)
+			return (-1);
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	todel = *head;
+	temp = *head;
+	for (; index > 0; index--)
+	{
+		if (todel == NULL)
+			return (-1);
+		temp = todel;
+		todel = todel->next;
+	}
+	if (todel->next)
+	{
+		temp->next = (temp->next)->next;
+	}
+	free(todel);
+	return (1);
+}
