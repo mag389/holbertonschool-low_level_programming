b9845bd2dfa36f73fc00d23271fc43284943632e
Michael George <mag389@cornell.edu>
2020-03-23 17:53:30 +0000

    fil to free list from double pointer instead of single

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* free_listint - frees the entire list
+* Return: void
+* @head: the current head node of the list
+*/
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+		return;
+	free_listint(*head);
+	*head = NULL;
+}
+#include "lists.h"
+
+/**
+* free_listint - frees the entire list
+* Return: void
+* @head: the current head node of the list
+*/
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	if (head->next)
+		free_listint(head->next);
+	free(head);
+}
