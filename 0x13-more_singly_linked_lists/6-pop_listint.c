2a935cfc057889ae719367ae9926642212db5929
Michael George <mag389@cornell.edu>
2020-03-23 18:05:47 +0000

    pops off and returns head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* pop_listint - removes head and gives first value
+* Return: the integer value in the head
+* @head: the location of the head node
+*/
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+
+	return (n);
+}
