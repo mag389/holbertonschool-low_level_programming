5d0178578cf4b0d832ba3c2aa253e6d4ab20dcdd
Michael George <mag389@cornell.edu>
2020-03-24 15:56:15 +0000

    making sure it works for adding to end of list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		templast = temp;
 		temp = temp->next;
 	}
-	if (!temp)
+	if (!templast)
 	{
 		free(newnode);
 		return (NULL);
