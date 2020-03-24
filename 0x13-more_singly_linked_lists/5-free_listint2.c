2e00037afbb3bd22cf20b5e265dddf006b52b2e8
Michael George <mag389@cornell.edu>
2020-03-24 04:39:16 +0000

    fixing betty mistake

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
-* free_listint - frees the entire list
+* free_listint2 - frees the entire list
 * Return: void
-* @head: the current head node of the list
+* @head: pointer to head node of the list
 */
 void free_listint2(listint_t **head)
 {
