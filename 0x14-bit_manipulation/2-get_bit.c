09b850fe92a03d38c403d0fb5f29d837fbc6ab70
Michael George <mag389@cornell.edu>
2020-03-26 17:52:21 +0000

    returns the bit at an index, betty compliant tested local

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* get_bit - return bit at index
+* Return: value of the bit or -1 for errors
+* @n: the number
+* @index: the index of the bit in the number
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int idx;
+
+	if (index > 64)
+		return (-1);
+	idx = 1 << index;
+	if (idx > n)
+		return (-1);
+	return ((n & idx) == idx);
+}
