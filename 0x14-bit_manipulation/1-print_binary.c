20995fb8e5d0a1ff8df31bb4e8b619f324b71d0f
Michael George <mag389@cornell.edu>
2020-03-26 21:54:30 +0000

    fixing issue where it wouldn't work for max luint, instead of digits >> 1 after does a cpy >> 1 before

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,14 +10,23 @@ void print_binary(unsigned long int n)
 	unsigned long int cpy = n, digits = 1;
 	int max_digits = 0;
 
-	while (cpy != 0 && max_digits < 64)
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	cpy = cpy >> 1;
+	while (cpy != 0 && max_digits < 65)
 	{
 		digits = digits << 1;
 		cpy = cpy >> 1;
 		max_digits++;
+/*		printf("digits:%lu\n", digits);*/
 	}
-	if (n > 0)
-		digits = digits >> 1;
+/*	printf("n: %lu\n", n);
+	printf("max_dig: %i\n", max_digits);
+	printf("digits: %lu\n", digits);
+	digits = digits >> 1;*/
 	while (digits != 0)
 	{
 		_putchar('0' + ((n & digits) == digits));
