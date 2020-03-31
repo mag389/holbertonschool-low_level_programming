d47a1cd9dbea37733c01718d5b7b9e05c1507290
Michael George <mag389@cornell.edu>
2020-03-31 17:13:50 +0000

    added a different check for write failure

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(filenum);
 	numwritten = write(1, buf, numread);
 	free(buf);
-	if (numwritten != numread)
+	if (numwritten != numread || numwritten == -1)
 		return (0);
 	return (numwritten);
 
