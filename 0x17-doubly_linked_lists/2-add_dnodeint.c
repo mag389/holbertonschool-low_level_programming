33e9bc143119b373f4ae02a6bc310518dc68ffb2
mag389 <mag389@cornell.edu>
2020-05-02 20:01:22 +0000

    adding add_node function file, updated header for mandatory tasks, betty checked

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* add_dnodeint - adds new dnode
+* Return: address of new element or NULL
+* @head: the head node
+* @n: the new int to add
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newhead;
+
+	newhead = malloc(sizeof(dlistint_t));
+	if (!newhead)
+		return (NULL);
+	newhead->n = n;
+	newhead->prev = NULL;
+	newhead->next = *head;
+	*head = newhead;
+
+	return (newhead);
+}
