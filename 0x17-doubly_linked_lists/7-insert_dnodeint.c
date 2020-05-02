3d6ce18bac9b3ebebb307a1eb267dc3b388dbbd2
mag389 <mag389@cornell.edu>
2020-05-02 21:21:14 +0000

    update heads last when adding index 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		newnode->next = *h;
+		if (*h)
+			h->prev = newnode;
 		*h = newnode;
 		return (newnode);
 	}
