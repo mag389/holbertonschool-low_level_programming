9aaff9593c6bd60677704894b86e22d20be3ecc3
mag389 <mag389@cornell.edu>
2020-05-02 21:22:27 +0000

    h->prev should have been (*h)->prev

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		newnode->next = *h;
 		if (*h)
-			h->prev = newnode;
+			(*h)->prev = newnode;
 		*h = newnode;
 		return (newnode);
 	}
