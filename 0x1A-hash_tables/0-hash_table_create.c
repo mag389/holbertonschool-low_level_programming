6032e5ed8f548adb677530c2b797c0a23aebdd87
mag389 <mag389@cornell.edu>
2020-05-28 16:34:37 +0000

    function for creating hash table, readme and header, betty compliant

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - creates a hash table
+* Return: pointer to new table or null
+* @size: the size of the array
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	return (ht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,6 @@
+# Hash tables
+The use and implementation of hash tables in C
+
+0. function to create hash tables
+
+1. implementing the djb2 algorithm
