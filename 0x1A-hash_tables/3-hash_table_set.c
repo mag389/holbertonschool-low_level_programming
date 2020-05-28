da72d12cb58e4615ccb569762fa10bdc2243f036
mag389 <mag389@cornell.edu>
2020-05-28 22:29:53 +0000

    finally made progress to what i believe works, not including edge cases. as of now not betty.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,13 +11,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newhn, *head;
 	char *dupstr;
-	int i;
 	unsigned long int hashindex;
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || value == NULL || !(*key))
 		return (0);
-	(void)head;
 	hashindex = key_index((const unsigned char *) key, ht->size);
+
+	head = ht->array[hashindex];
+	printf("the head %p\n",(void*) head);
+	if (head)
+		printf("the head exists ----------\n");
+	while (head)
+	{
+		printf("checking for dupkeys\n");
+		if (!strcmp(key, head->key))
+		{
+			printf("keymatch\n");
+			free(head->value);
+			head->value = strdup(value);
+			if (head->value == NULL)
+				return (0);
+			return(1);
+		}
+		head = head->next;
+	}
 
 	newhn = malloc(sizeof(hash_node_t));
 	if (newhn == NULL)
@@ -28,14 +45,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newhn->key = strdup(key);
 	newhn->value = dupstr;
 	newhn->next = NULL;
-
-	printf("right before the hasindex f'n %s\n", dupstr);
-	if ((ht->array)[hashindex] == NULL)
-		(ht->array)[hashindex] = newhn;
+	head = (ht->array)[hashindex];
+	printf("right before the index:%lu f'n %s, %s\n",hashindex,  dupstr, newhn->key);
+	if (ht->array[hashindex] == NULL)
+	{
+		printf("inf the if\n");
+		ht->array[hashindex] = newhn;
+	}
 	else
 	{
 		newhn->next = (ht->array)[hashindex];
 		(ht->array)[hashindex] = newhn;
 	}
+	printf("done with the first one\n");
 	return (1);
 }
