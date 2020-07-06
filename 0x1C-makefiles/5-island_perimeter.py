7087210694deea095d9a4275a74713c69a103f1b
mag389 <mag389@cornell.edu>
2020-07-06 16:38:29 +0000

    calculates isalnd perimeter, currently no error checks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""the island perimeter problem"""
+
+
+def island_perimeter(grid):
+    """finds the total perimeter of the island"""
+
+    perimeter = 0
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] == 1:
+                if (i > 0 and grid[i - 1][j] == 0) or i == 0:
+                    perimeter += 1
+                if (j > 0 and grid[i][j - 1] == 0) or j == 0:
+                    perimeter += 1
+                if (i < len(grid) - 1 and grid[i + 1][j] == 0):
+                    perimeter += 1
+                if i == len(grid) - 1:
+                    perimeter += 1
+                if j < len(grid[i]) - 1 and grid[i][j + 1] == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1:
+                    perimeter += 1
+            print(grid[i][j], end="")
+        print(" " + str(perimeter))
+    return perimeter
