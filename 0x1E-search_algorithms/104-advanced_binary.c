844862c5303c1877497ecb5ba5365fb053cda5c9
mag389 <mag389@cornell.edu>
2020-08-26 20:17:14 +0000

    advanced binary

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,67 @@
+#include "search_algos.h"
+void print_array(int *array, size_t size);
+/**
+* advanced_binary - performs binary search for value in array
+* Return: first index where value is located, or -1
+* @array: pointer to first element of the array to search in, ASC unique
+* @size: total number of elements of the array
+* @value: the value to search for within the array
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+	unsigned int mid;
+	int temp;
+
+/*	find midpoint of array */
+	mid = size / 2 - 1 + size % 2;
+/*	Null check of array */
+	if (!array)
+		return (-1);
+/*	printf("mid is %u", mid);*/
+	print_array(array, size);
+/*
+*	deal with a size 1 basecase
+*	not strictly neccesary, useful for finding errors
+*/
+	if (size == 1)
+	{
+		if (array[0] == value)
+			return (0);
+		return (-1);
+	}
+/*	check midpoint for value compare, for recursive calls */
+	if (array[mid] > value)
+		return (advanced_binary(array, mid, value));
+	else if (array[mid] < value)
+	{
+		temp = advanced_binary((array + mid + 1), mid + 1 - size % 2, value);
+		if (temp < 0)
+			return (temp);
+		else
+			return (mid  + 1 + temp);
+	}
+	else if (array[mid] == value)
+	{
+		if (mid == 0)
+			return (mid);
+		return (advanced_binary(array, mid + 1, value));
+	}
+	else
+		return (-1);
+}
+
+/**
+* print_array - prints the array
+* Return: void
+* @array: pointer to first element
+* @size: how much to print
+*/
+void print_array(int *array, size_t size)
+{
+	unsigned int i;
+
+	printf("Searching in array: %i", array[0]);
+	for (i = 1; i < size; i++)
+		printf(", %i", array[i]);
+	printf("\n");
+}
